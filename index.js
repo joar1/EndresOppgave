7dbe9a524ed752d9f752cc43d871fda4cdc8ba93
Joar <joar1.kodehode@gmail.com>
2022-06-23 13:53:35 +0200

    fresh
    -

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 input = document.getElementById('input-el')
 display = document.getElementById('display-el')
 submitBtn = document.getElementById('submit-btn')
+numbers = document.getElementById('numbers')
+
 
 submitBtn.addEventListener('click', addZu);
+input.addEventListener('keydown', (e)=>{
+   if (e.key !== 'Enter') return
+      whatHaveIDone()
+      addZu()
+      input.value = ""
+      input.focus()
+      
+})
 
-
+function whatHaveIDone(){
+   numbers.textContent = "Du skrev: " + input.value
+}
 
 function addZu(){
    console.log('Click!')
    let val = input.value.split('');
    console.log(val)
    input.value = val.reduce((a,c)=> a + Number(c),0);
-   display.textContent = input.value;
+   display.textContent = "Summen blir: " + input.value;
 }
